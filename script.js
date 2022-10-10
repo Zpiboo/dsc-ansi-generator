7b5434009d653b70ae9d7b8f8c7a1a4a16549220
Le__Geek <lolmdrrvjv@gmail.com>
2022-10-10 12:03:29 +0000

    Added underlined and bold text

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ let prefix = (...args) => `\u001b[${args.join(";")}m`
 const toggles = document.querySelectorAll(".toolbar > .toggle");
 [...toggles].forEach(toggle => {
   toggle.addEventListener("click", e => {
-    e.target.classList.toggle("selected")
+    e.target.classList.toggle("selected");
   });
 });
