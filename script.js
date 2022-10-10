86209a6e7f5181f6676c9a31eef64d4b4f07235f
Le__Geek <lolmdrrvjv@gmail.com>
2022-10-10 10:32:18 +0000

    Added (background) colors

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 let prefix = (...args) => `\u001b[${args.join(";")}m`
 
-// Auto resize textarea
-const tx = document.getElementsByTagName("textarea");
-for (let i = 0; i < tx.length; i++) {
-  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
-  tx[i].addEventListener("input", OnInput, false);
-  window.addEventListener("load", e => {
-    OnInput({target:tx[i]});
-  }, false);
-}
-function OnInput(e) {
-  e.target.style.height = 0;
-  e.target.style.height = `${e.target.scrollHeight}px`;
-}
+
+const tools = document.querySelectorAll(".toolbar > *");
+[...tools].forEach(tool => {
+  tool.addEventListener("click", e => {
+    e.target.classList.toggle("selected")
+  });
+});
