fdcf84dfab61a365cf33a33b57180bc86c3b6651
zpiboo <zpiboo@zpiboos-computer.lan>
2022-10-11 06:59:19 +0200

    Started js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let prefix = (...args) => `\u001b[${args.join(";")}m`
 
 
-const toggles = document.querySelectorAll(".toolbar > .toggle");
-[...toggles].forEach(toggle => {
-  toggle.addEventListener("click", e => {
+const formatButtons = document.querySelectorAll(".toolbar > .format");
+formatButtons.forEach(btn => {
+  btn.addEventListener("click", e => {
     e.target.classList.toggle("selected");
   });
 });
