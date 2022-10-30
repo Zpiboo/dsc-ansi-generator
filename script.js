aa4e909a871ff6e07e3f72bd5b29d0c366463d4b
Zpiboo <zpiboo@outlook.com>
2022-10-30 17:13:06 +0100

    Started to try preventing 'insertParagraph' input event from happening

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let prefix = (...args) => `\u001b[${args.join(";")}m`
 
 const textArea = document.querySelector("div.text");
+textArea.addEventListener("beforeinput", preventParagraphInsert);
+
 const formatButtons = document.querySelectorAll(".toolbar > .format");
 formatButtons.forEach(btn => {
   btn.addEventListener("click", e => {
@@ -12,4 +14,15 @@ formatButtons.forEach(btn => {
 
 function modifyText() {
   // TODO: Finish that lol
+}
+
+
+function preventParagraphInsert(e) {
+  console.log(`Actual event: ${e}`);
+  if (e.inputType === "insertParagraph") {
+    e.preventDefault();
+    let newEvt = new InputEvent("beforeinput", { inputType: "insertText", data: "a", target: this, bubbles: true });
+    console.log(`My event: ${newEvt}`);
+    this.dispatchEvent(newEvt);
+  }
 }
